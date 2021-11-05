5e16b3f2c3725937133743dbdc6233cdc00ea6f2
Зубенко Михаил Петрович <70808686+Savka-bot@users.noreply.github.com>
2021-11-05 20:55:14 +0200

    stupid update

--- 1 progect/1 progect.cpp
+++ 1 progect/1 progect.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
+#include <list>
 using namespace std;
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 int main()
 {
@@ -23,8 +26,46 @@ int main()
     //........................................
 
     //........................................
+    cout << " 1 задача" << endl;
+    int main();
+    {
+        int sum = 0;
+        for (int i = 0; i <= 99; i++) {
+
+            sum += i;
+        }
+
+        cout << sum << endl;
+    }
+    //........................................
+    //cout << "Вывести на экран цифровой треугольник " << endl;
+    //int main();
+    //{
+        //int k = 7;
+        //for (int i = 1; i <= 5; i++)
+        //{
+            //for (int b = 1; b <= i; b++)
+                //cout << k << "  ";
+
+            //cout << endl;
+        //}
+
+    //}
+    //........................................
+    //cout << " Вычислить произведение элементов следующей последовательности " << endl;
+    //int main();
+    //{
+        //int sum = 0;
+        //for (i = 0; i <= sqrt(95); i++)
+        //{
+            //sum += i;
+        //}
+        //cout << sum << endl;
+    //}
 
 
-    return 0;
 }
+
+
+
 
